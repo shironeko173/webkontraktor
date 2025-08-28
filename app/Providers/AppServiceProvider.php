512bd1110a3a08cc839b16dfd0d8bb35b823a4c4
mhdfahrurozi <mhdfahrurozi5@gmail.com>
2025-08-29 00:17:09 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+            
+            // Juga force HTTPS untuk request
+            if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+                $_SERVER['HTTPS'] = 'on';
+            }
+        }
     }
 }
